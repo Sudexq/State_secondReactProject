8d1ae4ad3658e59289622fa9db141b54fffdd0b1
sudenurTilla <sudenur.tilla2020@gmail.com>
2024-03-22 16:21:12 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -3,31 +3,43 @@ import "./App.css";
 import { useState } from "react"; //useState i import ettik
 
 function getRandomCourse() {
-  const courseArray = ["Angular", "Bootstrap", "Frontend", "KompleWeb"];
+  const courseArray = ["Angular", "Bootstrap", "Frontend", "Kompleweb"];
   return courseArray[Math.floor(Math.random() * courseArray.length)];
   //Math.random() , 0 ve 1 arasında değerler döndürür
   //courseArray.length 0,1,2,3 olabilir
   //mesela Math.random() = 0.74 , courseArray.length = 2 , 0.74*2=1.48 oldu Math.floor da bunu 1 olarak alır ve 'Bootstrap' i döndürür
 }
 
-// <Course/> bununla birlikte course.js in içindekileri app.js görmesini sağlıyoruz
+// <Course/> bununla birlikte course.js in içindekileri app.js in görmesini sağlıyoruz
 function App() {
+  //useState hook'u kullanılarak componentin durumu yönetilir.
+  //courses adında bir durum ve bu durumu güncellemek için setCourses adında bir işlev vardır.
   //usestatesnippet
   const [courses, setCourses] = useState([]);
+
+  //bir butona tıkladığında çalışır ve setCourses aracılığıyla rastgele bir kurs ekler.
   const handleClick = () => {
-    setCourses([...courses, getRandomCourse()]); //önceki kurslarda kalsın diye ...courses bu ifadeyi kullanıyoruz
+    setCourses([...courses, getRandomCourse()]);
+    //Normalde, setCourses ile yeni bir kurs eklediğinizde, mevcut courses dizisini değiştirir ve eski verileri kaybedersiniz.
+    //Ancak ...courses ifadesi, eski kurslar dizisini kopyalayarak yeni bir dizi oluşturur.
+    //Böylece eski kurslar dizisini korumuş olursunuz ve yeni bir kurs eklenirken eski kurslar kaybolmaz.
+    //Yani, her yeni kurs eklemesi, mevcut kursların üzerine eklenir ve mevcut veriler kaybolmaz.
+    //Bu şekilde, mevcut verilerin kaybolmadığı bir güncelleme sağlanmış olur.
   };
-  const courseList=()=>{
-    courses.map((course, index) => {//her bir eleman course ve key hatası alacağından her bir course a index veriyoruz
-      return <Course key={index} courseName={course}/> //çünkü her bir course ın farklı bir key value su olmalı
-      //courseName bizim props umuzdur
-    })
-  }
+
+  //courses dizisindeki her bir kurs için bir <Course> componenti oluşturarak bir JSX dizisi oluşturuyor.
+  const courseList = courses.map((course, index) => {
+    return <Course key={index} CourseName={course} />;
+    //Her bir kurs için bir key prop'u atanır.
+    //çünkü her bir course ın farklı bir key value su olmalı
+    //courseName ve key bizim props umuzdur
+  });
+
   return (
     <div className="App">
-      <button onClick={handleClick}>Kurs Ekle</button>
+      <button className="appButton" onClick={handleClick}>Kurs Ekle</button>
       {/* jsx formatı */}
-      {courseList}
+      <div className="courseList">{courseList}</div>
     </div>
   );
 }
--- src/Course.js
+++ src/Course.js
@@ -1,15 +1,31 @@
-import Angular from './images/angular.png';
-import Bootstrap from './images/Bootstrap.png';
-import Frontend from './images/Frontend.png';
-import kompleweb from './images/kompleweb.jpg';
+import "./Course.css"; //course js e özel course css
+import Angular from "./images/angular.png";
+import Bootstrap from "./images/Bootstrap.png";
+import Frontend from "./images/Frontend.png";
+import Kompleweb from "./images/kompleweb.jpg";
 
-function Course({courseName}) {
-    console.log(courseName);
-    return (
-        <div>
-            <img src={Angular}/>
-        </div>
-      );
+const courseMap = {
+  //   //courseName:resmin ismi
+  //   Angular: Angular,
+  //   Bootstrap: Bootstrap,
+  //   Frontend: Frontend,
+  //   kompleweb: Kompleweb,
+  Angular,
+  Bootstrap,
+  Frontend,
+  Kompleweb,
+  //key value ve image ismi aynı olduğu için direkt bir tane yazsak yeter
+};
+
+function Course({ CourseName }) {
+  //   console.log(CourseName);
+  //   console.log(CourseName);
+  console.log(courseMap[CourseName]);
+  return (
+    <div className="course">
+      <img className="courseImg" src={courseMap[CourseName]} alt="course" />
+    </div>
+  );
 }
 
 export default Course;
